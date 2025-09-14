13dad59d7c179f0a38187a8c97778d6a610098d9
Singh-Harpal <harpalsingh31052004@gmail.com>
2025-09-14 19:01:14 +0530

    Add file 7

---
+++ section 1/Ques7.py
@@ -0,0 +1,7 @@
+#smallest of three numbers
+X = float(input("Enter first no: "))
+Y = float(input("Enter second no: "))
+Z = float(input("Enter third no: "))
+
+smallest = min(X, Y, Z)
+print("Smallest no. is : ",smallest)
