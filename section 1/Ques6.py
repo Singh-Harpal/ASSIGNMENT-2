59663254d1bc5ecc91e91af2b2ec2b0f97c1d9bb
Singh-Harpal <harpalsingh31052004@gmail.com>
2025-09-14 18:51:28 +0530

    Add file 6

---
+++ section 1/Ques6.py
@@ -0,0 +1,8 @@
+#To print largest of two numbers
+A = int(input("Enter first number : "))
+B = int(input("Enter second number : "))
+
+if A > B:
+    print("Large number:",A)
+else:
+    print("Large number:",B)
