86c8d967d168c61c6fd4ce6d64fba6b3a24352e5
Singh-Harpal <harpalsingh31052004@gmail.com>
2025-09-14 18:01:01 +0530

    Add file 5

---
+++ section 1/Ques5.py
@@ -0,0 +1,11 @@
+#check if number is positive, negative or zero
+N = int(input("Enter a number: "))
+
+if N > 0:
+    print("Positive number")
+
+elif N<0:
+    print("Negative number")
+
+else:
+    print("Zero")
