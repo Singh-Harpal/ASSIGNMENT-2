2a1f77d9aadaa8a00989958f30b7976b8b839957
Singh-Harpal <harpalsingh31052004@gmail.com>
2025-09-15 07:03:06 +0530

    Add file 28

---
+++ section 4/Ques27.py
@@ -0,0 +1,16 @@
+# To find all prime numbers b/w 1 to 100
+import math
+def is_prime(n):
+    if n < 2:
+        return False 
+    for i in range(2 , int(math.sqrt(n))+1 ):
+        if n % i == 0:
+            return False
+    return True
+
+
+print("Find prime number b/w 1 and 100")
+
+for num in range(1 , 101):
+    if is_prime(num):
+        print(num , end =' ')
